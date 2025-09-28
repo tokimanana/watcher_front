a837b84fe8c9f95724f5e962d9d8a5639cbd5213
tokimanana <tokimananasarobidy@gmail.com>
2025-09-28 18:03:52 +0300

    feat: implement authentication guard and update auth interceptor

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,5 +1,18 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { Router, CanActivateFn } from '@angular/router';
+import { AuthService } from '../../features/auth/services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.isAuthenticated()) {
+    return true;
+  }
+
+  return router.createUrlTree(['/auth/login'], {
+    queryParams: {
+      returnUrl: state.url
+    }
+  });
 };
